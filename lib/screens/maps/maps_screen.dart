a04d3b00d54ddd4a54c04b2ed5124987c4fd7b2f
RezaAzharii <rezaazhari867@gmail.com>
2025-05-23 00:19:56 +0700

    Inisialisasi MapsPage dengan controller, marker, posisi, dan alamat menggunakan Google Maps dan Geolocator

---
+++ lib/screens/maps/maps_screen.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:plantapp/constants.dart';
+
+class MapsPage extends StatefulWidget {
+  const MapsPage({super.key});
+
+  @override
+  State<MapsPage> createState() => _MapsPageState();
+}
+
+class _MapsPageState extends State<MapsPage> {
+  final Completer<GoogleMapController> _ctrl = Completer();
+  Marker? _pickedMarker;
+  String? _pickedAddress;
+  String? _currentAddress;
+  CameraPosition? _initialCamera;
+  Position? _currentPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
