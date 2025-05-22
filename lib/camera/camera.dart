125f22411ecb4d0d49aa10e66d4bbbbed1e2b7c2
RezaAzharii <rezaazhari867@gmail.com>
2025-05-23 00:05:31 +0700

    Menambahkan CameraPage dengan inisialisasi controller dan variabel state

---
+++ lib/camera/camera.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:plantapp/constants.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late List<CameraDescription> _cameras;
+  CameraController? _controller;
+  int _selectedCameraIdx = 0;
+  FlashMode _flashMode = FlashMode.off;
+  double _zoom = 1.0;
+  double _minZoom = 1.0;
+  double _maxZoom = 1.0;
+  bool _isZoomSupported = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
