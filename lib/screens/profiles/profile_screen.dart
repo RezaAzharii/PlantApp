a26e7a86c57444ac492080c1c991ecde2702cb5a
RezaAzharii <rezaazhari867@gmail.com>
2025-05-23 00:35:03 +0700

    Menambahkan kerangka awal ProfileScreen dengan input alamat dan gambar profil

---
+++ lib/screens/profiles/profile_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/maps/maps_screen.dart';
+import 'package:plantapp/camera/camera.dart';
+import 'package:plantapp/screens/profiles/storage_helper.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfileScreen extends StatefulWidget {
+  final File? profileImage;
+  final String? initialAddress;
+
+  const ProfileScreen({super.key, this.profileImage, this.initialAddress});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  File? _imageProfile;
+  String? _alamatDipilih;
+  final TextEditingController _provinsiController = TextEditingController();
+  final TextEditingController _kodePosController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        
+      ),
+    );
+  }
+}
