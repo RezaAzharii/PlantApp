5200d42299a1a29f66dde07222e5da08b11eb361
RezaAzharii <rezaazhari867@gmail.com>
2025-05-23 00:11:03 +0700

    Menambahkan fungsi untuk toggleflash

--- lib/camera/camera.dart
+++ lib/camera/camera.dart
@@ -70,6 +70,17 @@ class _CameraPageState extends State<CameraPage> {
     await _setupCamera(nextIndex);
   }
 
+  void _toggleFlash() async {
+    FlashMode next =
+        _flashMode == FlashMode.off
+            ? FlashMode.auto
+            : _flashMode == FlashMode.auto
+            ? FlashMode.always
+            : FlashMode.off;
+    await _controller!.setFlashMode(next);
+    setState(() => _flashMode = next);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
